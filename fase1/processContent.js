27d7dc4ccc34955df5119b8efadb406239a8ea78
Nietsnie-beep <al02982380@tecmilenio.mx>
2024-03-01 20:27:12 -0600

    actividades

--- fase1/processContent.js
+++
@@ -1,7 +0,0 @@
-// processContent.js
-export const removeHtmlTags = (content) => content.replace(/<[^>]*>/g, '');
-
-export const extractAndSortWords = (content) => {
-    const words = content.split(/\s+/).filter(word => word.length > 0);
-    return words.sort();
-};
--- fase1/writeOutput.js
+++
@@ -1,6 +0,0 @@
-// writeOutput.js
-import fs from 'fs';
-
-export const writeToFile = (filePath, content) => {
-    fs.writeFileSync(filePath, content, 'utf8');
-};
